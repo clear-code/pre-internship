f587d53560ccb808f7cdaf2fef109ccc298f3ae6
Takuro Ashie <ashie@clear-code.com>
2016-03-07 20:52:23 +0900

    スタイル修正の例1
    * 代入演算子や算術演算子の前後にスペースを入れる
    * ifやelseの前後にスペースを入れる
    * カンマ(",")の後にスペースを入れる
    * "{" や "}"の前後にスペースを入れる

--- 2016-03-01/c1.c
+++ 2016-03-01/c1.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int kokugo,shakai,rika;
-    int goukei,heikin;
+    int kokugo, shakai, rika;
+    int goukei, heikin;
     
     printf("国語の点数=");
-    scanf("%d",&kokugo);
+    scanf("%d", &kokugo);
     printf("社会の点数=");
-    scanf("%d",&shakai);
+    scanf("%d", &shakai);
     printf("理科の点数=");
-    scanf("%d",&rika);
+    scanf("%d", &rika);
     
-    goukei =kokugo+shakai+rika;
-    heikin =goukei/3;
+    goukei = kokugo + shakai + rika;
+    heikin = goukei / 3;
     
     printf("合計=%d 平均=%d\n", goukei, heikin);
     return 0;
--- 2016-03-01/c3.c
+++ 2016-03-01/c3.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int kubun,ninzu,goukei;
+    int kubun, ninzu, goukei;
     
     printf("区分＝");
     scanf("%d", &kubun);
     printf("人数＝");
     scanf("%d", &ninzu);
     
-    if(kubun==1){
-        goukei =ninzu*800;
-    }else{
-        goukei= ninzu*1500;
+    if (kubun == 1) {
+        goukei = ninzu * 800;
+    } else {
+        goukei = ninzu * 1500;
     }
     
     printf("料金合計＝%d\n", goukei);
